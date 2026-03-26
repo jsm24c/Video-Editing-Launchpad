c1ed2cf754c2d975c666ac07c03ceee19a856354
jsm24c <xmartjs@gmail.com>
2026-03-26 02:13:18 -0400

    added sfx tile

--- app.jsx
+++ app.jsx
@@ -9,7 +9,8 @@ const TILES = [
   { id: 'font', name: 'Font Picker', url: 'https://www.dafont.com/top.php', image: 'font.png' },
   { id: 'y2mate', name: 'Youtube Video Downloader', url: 'https://v4.www-y2mate.com/', image: 'y2mate.png' },
   { id: 'luts', name: 'LUTs', url: 'https://freshluts.com/luts', image: 'luts.png' },
-  { id: 'howto', name: "How-To's", url: 'https://docs.google.com/document/d/12-C1Reu0s2bdL1LuW2Q5QjQM2PvrRY6flwyZX9jFI20/edit?tab=t.0', image: 'how to.png' }
+  { id: 'howto', name: "How-To's", url: 'https://docs.google.com/document/d/12-C1Reu0s2bdL1LuW2Q5QjQM2PvrRY6flwyZX9jFI20/edit?tab=t.0', image: 'how to.png' },
+  { id: 'SFX', name: "SFX", url: 'https://pixabay.com/', image: 'how to.png' }
 ];
 
 // 2. Tile — one link box (receives name, url, image as props)
